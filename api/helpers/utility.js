1e929ec07c4382683eba741fd0860212e23781bf
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-11-02 11:49:37 +0200

    write sort, median and filter functions

--- api/helpers/utility.js
+++ api/helpers/utility.js
@@ -45,5 +45,55 @@ const shuffle = (array) => {
   return array;
 };
 
+const partition = (start, end, array) => {
+  let pivotIndex = start;
+  let pivot = array[pivotIndex];
+
+  while (start < end) {
+    while (start < array.length && array[start] <= pivot) start += 1;
+
+    while (array[end] > pivot) end -= 1;
+
+    if (start < end) [array[start], array[end]] = [array[end], array[start]];
+  }
+
+  [array[end], array[pivotIndex]] = [array[pivotIndex], array[end]];
+
+  return end;
+};
+
+const quickSort = (start, end, array) => {
+  if (start < end) {
+    let pivot = partition(start, end, array);
+
+    quickSort(start, pivot - 1, array);
+    quickSort(pivot + 1, end, array);
+  }
+};
+
+const sort = (array) => quickSort(0, array.length - 1, array);
+
+const calcMedian = (array) => {
+  sort(array);
+
+  const center = array[Math.floor(array.length / 2)];
+
+  if (array.length % 2 === 1) return center;
+
+  return (center + array[Math.floor(array.length / 2) - 1]) / 2;
+};
+
+const filter = (array, conditionFunc) => {
+  let filteredArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (conditionFunc(array[i])) filteredArray.push(array[i]);
+  }
+
+  return filteredArray;
+};
+
 module.exports.requestStreamData = requestStreamData;
 module.exports.shuffle = shuffle;
+module.exports.calcMedian = calcMedian;
+module.exports.filter = filter;
