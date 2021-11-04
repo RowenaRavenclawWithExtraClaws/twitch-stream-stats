0b4aa4b91f65aaae3699618383c3193f7fd7904f
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-11-04 16:26:57 +0200

    add custom menue component

---
+++ webapp/components/customMenu.tsx
@@ -0,0 +1,73 @@
+import { List, ListItem, ListItemText, Menu, MenuItem } from "@mui/material";
+import { MouseEvent, useState } from "react";
+
+const CustomMenue = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const open = Boolean(anchorEl);
+  const options = ["Database", "In-memory"];
+
+  const handleClickListItem = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItemClick = (
+    event: MouseEvent<HTMLElement>,
+    index: number
+  ) => {
+    setSelectedIndex(index);
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <List
+        component="nav"
+        aria-label="Device settings"
+        sx={{ bgcolor: "background.paper" }}
+        style={{ width: 150 }}
+      >
+        <ListItem
+          button
+          id="lock-button"
+          aria-haspopup="listbox"
+          aria-controls="lock-menu"
+          aria-label="when device is locked"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClickListItem}
+        >
+          <ListItemText
+            primary="Data Source"
+            secondary={options[selectedIndex]}
+          />
+        </ListItem>
+      </List>
+      <Menu
+        id="lock-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "lock-button",
+          role: "listbox",
+        }}
+      >
+        {options.map((option, index) => (
+          <MenuItem
+            key={option}
+            selected={index === selectedIndex}
+            onClick={(event) => handleMenuItemClick(event, index)}
+          >
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+};
+
+export default CustomMenue;
