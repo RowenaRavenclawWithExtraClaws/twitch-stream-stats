70b9c97b622dcb9c2f7b4e87af0b94e5f1f1d377
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-11-04 22:02:54 +0200

    import new styles

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "../styles/_preloader.scss";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "../redux/store";
