c25b2a3b4c6268b530ebb9258b927659f4c1f25d
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-11-04 21:53:38 +0200

    use redux selector to load data

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -1,43 +1,55 @@
 import type { NextPage } from "next";
-import {
-  Grid,
-  Container,
-  Card,
-  CardContent,
-  Typography,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-} from "@mui/material";
+import { Grid, Container } from "@mui/material";
 import CustomFooter from "../components/customFooter";
 import CustomHeader from "../components/customHeader";
 import styles from "../styles/Home.module.css";
-import { Table } from "reactstrap";
 import StatsCard from "../components/statsCard";
+import { useSelector } from "react-redux";
+import { selectStreamsPerGame } from "../redux/streamsPerGameSlice";
+import { selectViewersPerGame } from "../redux/viewersPerGameSlice";
+import { selectStreamsOddViewers } from "../redux/streamsOddViewersSlice";
+import { selectStreamsEvenViewers } from "../redux/streamsEvenViewersSlice";
+import { selectStreamsTop100 } from "../redux/streamsTop100Slice";
+import { selectStreamsSameViewers } from "../redux/streamsSameViewersSlice";
 
 const Home: NextPage = () => {
+  const streamsPerGame = useSelector(selectStreamsPerGame);
+  const viewersPerGame = useSelector(selectViewersPerGame);
+  const streamsOddViewers = useSelector(selectStreamsOddViewers);
+  const streamsEvenViewers = useSelector(selectStreamsEvenViewers);
+  const streamsTop100 = useSelector(selectStreamsTop100);
+  const streamsSameViewers = useSelector(selectStreamsSameViewers);
+
   const stats = [
     {
       title: "Top streams per game",
-      data: [{ game_name: "Minecraft", stream_count: 10 }],
+      data: streamsPerGame.data,
+      pageCount: streamsPerGame.page_count,
     },
     {
       title: "Highest viewers per game",
-      data: [{ game_name: "Fifa 22", viewer_count: 89002 }],
+      data: viewersPerGame.data,
+      pageCount: viewersPerGame.page_count,
     },
     {
       title: "Streams with odd no. of viewers",
-      data: [{ title: "Im playing NBA 2k, just hop in", viewer_count: 8091 }],
+      data: streamsOddViewers.data,
+      pageCount: streamsOddViewers.page_count,
     },
     {
       title: "Streams with even no. of viewers",
-      data: [{ title: "Im playing Fifa 22, just hop in", viewer_count: 12590 }],
+      data: streamsEvenViewers.data,
+      pageCount: streamsEvenViewers.page_count,
     },
     {
       title: "Streams with the same no. of viewers",
-      data: [{ title: "Im playing NBA 2k, just hop in", viewer_count: 8091 }],
+      data: streamsSameViewers.data,
+      pageCount: streamsSameViewers.page_count,
+    },
+    {
+      title: "Top 100 streams",
+      data: streamsTop100.data,
+      pageCount: streamsTop100.page_count,
     },
   ];
 
@@ -49,7 +61,11 @@ const Home: NextPage = () => {
         <Grid container spacing={2}>
           {stats.map((info, indx) => (
             <Grid key={indx} item lg={4}>
-              <StatsCard title={info.title} data={info.data} />
+              <StatsCard
+                title={info.title}
+                data={info.data}
+                pageCount={info.pageCount}
+              />
             </Grid>
           ))}
         </Grid>
