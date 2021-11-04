cf1c4e0b0e8824004433be0bf01ae99a5017142f
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-11-04 21:53:50 +0200

    use data from selectors

--- webapp/components/statsCard.tsx
+++ webapp/components/statsCard.tsx
@@ -11,8 +11,13 @@ import {
 } from "@mui/material";
 import CustomMenue from "./customMenu";
 import CustomPagination from "./customPagination";
+import EmptyCard from "./emptyCard";
 
-const StatsCard = (props: { title: string; data: Array<object> }) => {
+const StatsCard = (props: {
+  title: string;
+  data: Array<object>;
+  pageCount: number;
+}) => {
   return (
     <Card style={{ marginTop: 20 }}>
       <CardContent>
@@ -22,29 +27,35 @@ const StatsCard = (props: { title: string; data: Array<object> }) => {
           </Typography>
           <CustomMenue />
         </div>
-        <TableContainer>
-          <Table size="small" aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                {Object.keys(props.data[0]).map((field, indx) => (
-                  <TableCell key={indx} style={{ fontWeight: "bold" }}>
-                    {field}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {props.data.map((info, indx1) => (
-                <TableRow key={indx1}>
-                  {Object.values(info).map((val, indx2) => (
-                    <TableCell key={indx2}>{val}</TableCell>
+        {!props.data.length ? (
+          <EmptyCard />
+        ) : (
+          <>
+            <TableContainer>
+              <Table size="small" aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    {Object.keys(props.data[0]).map((field, indx) => (
+                      <TableCell key={indx} style={{ fontWeight: "bold" }}>
+                        {field}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {props.data.map((info, indx1) => (
+                    <TableRow key={indx1}>
+                      {Object.values(info).map((val, indx2) => (
+                        <TableCell key={indx2}>{val}</TableCell>
+                      ))}
+                    </TableRow>
                   ))}
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <CustomPagination />
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <CustomPagination pageCount={props.pageCount} />
+          </>
+        )}
       </CardContent>
     </Card>
   );
